0e599a3a1930fe09464db32c7b15f4e1fc810080
erc83 <espinoza.eric@gmail.com>
2025-02-12 16:57:37 -0300

    add: config initial routes products gategay

--- src/products/products.controller.ts
+++ src/products/products.controller.ts
@@ -1,7 +1,36 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, Patch, Post, Delete, Body } from '@nestjs/common';
 
 
 @Controller('products')
 export class ProductsController {
   constructor() {}
+
+  @Post()
+  createProduct() {
+    return 'crea un producto'
+  }
+
+  @Get()
+  findAllProducts() {
+    return 'Esta funcion regresa varios productos'
+  }
+
+  @Get(':id')
+  findOne(@Param('id') id: string ) {
+    return 'Esta funcion regresa el producto' + id; 
+  }
+
+  @Patch(':id')
+  updateProduct(
+      @Param('id') id: string,
+      @Body() body: any
+    ) {
+    return 'Esta funcion actualiza el producto: ' + id
+  }
+
+  @Delete(':id')
+  DeleleProduct(@Param('id') id: string) {
+    return 'Esta funcion elimina el producto: ' + id
+  }
+
 }
