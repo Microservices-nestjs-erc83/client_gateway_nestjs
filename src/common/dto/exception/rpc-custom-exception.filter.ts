a38c2cab60fd61ae3e3adaaf37a4e329bbfaf98d
erc83 <espinoza.eric@gmail.com>
2025-02-12 20:18:36 -0300

    fix: implement custom exception filter

--- src/common/dto/exception/rpc-custom-exception.filter.ts
+++ src/common/dto/exception/rpc-custom-exception.filter.ts
@@ -11,9 +11,17 @@ export class RpcCustomExceptionFilter implements ExceptionFilter {
 
         const response = ctx.getResponse();
 
-        response.status(401).json({
-            status: 401,
-            message: 'Hola Mundo, Saludos a todos!!'
+        const rpcError = exception.getError()
+        //console.log(rpcError)
+
+        if( typeof rpcError === 'object' && 'status' in rpcError && 'message' in rpcError ) {
+            const status = isNaN(+rpcError.status) ? 400 : +rpcError.status;
+            return response.status(status).json(rpcError)
+        }
+
+        response.status(400).json({
+            status: 400,
+            message: rpcError,
         })
 
     }
