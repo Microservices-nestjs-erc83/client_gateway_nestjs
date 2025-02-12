272f57d075de513224d1a91fa1683b83685d25d4
erc83 <espinoza.eric@gmail.com>
2025-02-12 16:34:51 -0300

    add: cofiguration env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+import { envs } from './config/envs';
 
 async function bootstrap() {
+  const logger = new Logger('Main-Gateway')
+
   const app = await NestFactory.create(AppModule);
   await app.listen(process.env.PORT ?? 3000);
+
+  logger.log(`Gateway running on port ${envs.port}`)
+
 }
 bootstrap();
