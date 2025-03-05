1f73f245021aeda441a05105a206026b33c03ebc
erc83 <espinoza.eric@gmail.com>
2025-03-04 22:45:59 -0300

    add: pagination and filter alternative for status

--- src/orders/orders.controller.ts
+++ src/orders/orders.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get, Post, Body, Param, Inject, ParseUUIDPipe, Query } from '@nestjs/common';
-import { CreateOrderDto } from './dto';
+import { CreateOrderDto, StatusDto } from './dto';
 import { ORDER_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { catchError, firstValueFrom } from 'rxjs';
@@ -48,17 +48,17 @@ export class OrdersController {
   }
 
   @Get('status/:status')
-  async findAllByStatus(@Param('status' ) status: string, @Query()  paginationDto : PaginationDto) {
-
+  async findAllByStatus(
+    @Param() statusDto: StatusDto, 
+    @Query()  paginationDto : PaginationDto
+  ) {
     try {
 
-      return { status, paginationDto }
+      return this.orderClientMicro.send('findAllOrders', {
+        ...paginationDto,
+        status: statusDto.status,
+      })
 
-      //const order = await firstValueFrom(
-      //  this.orderClientMicro.send('findOneOrder', { id })
-      //)
-
-      //return order;
     } catch (error) {
       throw new RpcException( error )
     }
