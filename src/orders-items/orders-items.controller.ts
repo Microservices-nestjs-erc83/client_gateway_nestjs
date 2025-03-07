dca7bad2cc2fadaf3e459faaf8b02a49d1b6bf16
erc83 <espinoza.eric@gmail.com>
2025-03-07 12:26:28 -0300

    fix: changeStatus Order item

--- src/orders-items/orders-items.controller.ts
+++ src/orders-items/orders-items.controller.ts
@@ -7,7 +7,8 @@ import {
   Param,
   ParseIntPipe,
   ParseUUIDPipe,
-  Query
+  Query,
+  Patch
 } from '@nestjs/common';
 import { CreateOrdersItemDto } from './dto/create-orders-item.dto';
 
@@ -16,6 +17,7 @@ import { ORDER_ITEM_SERVICE } from 'src/config';
 import { ClientProxy, RpcException } from '@nestjs/microservices';
 import { firstValueFrom } from 'rxjs';
 import { OrderItemPaginationDto } from './dto/orderItem-pagination.dto';
+import { StatusItemDto } from './dto/status.dto';
 
 @Controller('orders-items')
 export class OrdersItemsController {
@@ -46,6 +48,21 @@ export class OrdersItemsController {
   findAll( @Query() orderItemPaginationDto: OrderItemPaginationDto ) {
       return this.orderItemClient.send('findAllOrdersItems',  orderItemPaginationDto)
       //return this.orderClientMicro.send('findAllOrders', {});
+  }
+
+  @Patch(':id')
+  changeStatus(
+    @Param('id', ParseUUIDPipe ) id : string,
+    @Body() statusItemDto : StatusItemDto,
+  ) {
+    try {
+      return this.orderItemClient.send('changeOrderItemStatus', {
+        id,
+        status: statusItemDto.status
+      })
+    } catch ( error ) {
+      throw new RpcException( error )
+    }
   }
 
 }
