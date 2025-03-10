e8d562404c9ad069df7bacb89504c2b1917d4e23
erc83 <espinoza.eric@gmail.com>
2025-03-09 23:58:54 -0300

    add: custom nats.module.ts

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ProductsModule } from './products/products.module';
 import { OrdersModule } from './orders/orders.module';
 import { OrdersItemsModule } from './orders-items/orders-items.module';
+import { NatsModule } from './transports/nats.module';
 
 @Module({
-  imports: [ProductsModule, OrdersModule, OrdersItemsModule],
+  imports: [ProductsModule, OrdersModule, OrdersItemsModule, NatsModule],
 })
 export class AppModule {}
--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,24 +1,13 @@
 import { Module } from '@nestjs/common';
-import { ClientsModule, Transport } from '@nestjs/microservices';
 
 import { ProductsController } from './products.controller';
-import { envs, NATS_SERVICE } from 'src/config';
+import { NatsModule } from 'src/transports/nats.module';
 
 @Module({
   controllers: [ProductsController],
   providers: [],
   imports:[
-    ClientsModule.register([
-      { 
-        name: NATS_SERVICE, 
-        transport: Transport.NATS,
-        options: {
-          //host: envs.productsMicroserviceHost,
-          //port: envs.productsMicroservicePort
-          servers: envs.natsServers,
-        },
-      }  
-    ])
+    NatsModule,
   ]
 })
 export class ProductsModule {
